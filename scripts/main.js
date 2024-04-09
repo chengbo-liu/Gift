3ba16c6db1a6f5e8c813c8fc135fc9a23269e037
chengbo.liu <112562157+chengbo-liu@users.noreply.github.com>
2024-04-09 11:26:34 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 var qrcode = new QRCode(document.getElementById("qrcode"), {
-  text: "http://www.example.com",
+  text: "https://chengbo-liu.github.io/Gift/",
   width: 256,
   height: 256,
   colorDark: "#000000", 
